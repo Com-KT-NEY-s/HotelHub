4440650553a07e55fc09bdd4c800b2ab3b4eb659
Wesw3s <151688352+Wesw3s@users.noreply.github.com>
2024-11-26 00:27:18 +0000

    upload do hotelhub semicompleto

--- src/main/java/Classes/Quartos.java
+++ src/main/java/Classes/Quartos.java
@@ -8,8 +8,8 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Quartos {
 
+public class Quartos {
     String tipo, numero;
     double preco;
     boolean disponivel;
@@ -45,7 +45,6 @@ public class Quartos {
     public void setDisponivel(boolean disponivel) {
         this.disponivel = disponivel;
     }
-
     public void criar() {
         String sql = "INSERT INTO quartos (numero, tipo, disponivel, preco) VALUES (?, ?, ?, ?)";
         try (Connection conn = Database.getConnection(); PreparedStatement pstmt = conn.prepareStatement(sql)) {
@@ -53,21 +52,21 @@ public class Quartos {
             pstmt.setString(2, this.tipo);
             pstmt.setBoolean(3, this.disponivel);
             pstmt.setDouble(4, this.preco);
-
+            
             pstmt.execute();
-        } catch (SQLException e) {
+        }
+        catch (SQLException e) {
             e.printStackTrace();
         }
-
+        
     }
-
-    public String[][] ler() {
+        public String[][] ler() {
         String sql = "SELECT * FROM quartos";
         List<String[]> linhas = new ArrayList<>();
-
-        try (Connection conn = Database.getConnection(); PreparedStatement pstm = conn.prepareStatement(sql); ResultSet rs = pstm.executeQuery()) {
+        
+        try(Connection conn = Database.getConnection(); PreparedStatement pstm = conn.prepareStatement(sql); ResultSet rs = pstm.executeQuery()) {
             int colunas = rs.getMetaData().getColumnCount();
-
+            
             while (rs.next()) {
                 String[] linha = new String[colunas];
                 for (int i = 1; i <= colunas; i++) {
@@ -75,18 +74,14 @@ public class Quartos {
                 }
                 linhas.add(linha);
             }
-        } catch (SQLException e) {
+        }
+        catch (SQLException e) {
             e.printStackTrace();
         }
         String[][] resultado = new String[linhas.size()][];
         resultado = linhas.toArray(resultado);
-
+        
         return resultado;
     }
-
-    public static ArrayList<Quartos> listarQuartos() {
-        ArrayList<Quartos> lista = new ArrayList<>();
-
-        return lista;
-    }
 }
+    
--- src/main/java/Classes/Usuarios.java
+++ src/main/java/Classes/Usuarios.java
@@ -1,13 +1,36 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package Classes;
 
-/**
- *
- * @author WESLEYLUCASMOREIRA
- */
+import DataBase.Database;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class Usuarios {
-    
+
+    private String nome, idade, cpf, senha;
+
+    public Usuarios(String nome, String idade, String cpf, String senha) {
+        this.nome = nome;
+        this.idade = idade;
+        this.cpf = cpf;
+        this.senha = senha;
+    }
+
+    public void inserirUser(String nome, int idade, String cpf, String senha) {
+        Connection conn = Database.getConnection();
+        try {
+            PreparedStatement stmt = conn.prepareStatement("INSERT INTO usuarios (nome, idade, cpf, senha) VALUES (?, ?, ?, ?)");
+            stmt.setString(1, nome);
+            stmt.setInt(2, idade);
+            stmt.setString(3, cpf);
+            stmt.setString(4, senha);
+
+            stmt.execute();
+
+        } catch (SQLException ex) {
+            Logger.getLogger(Usuarios.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
 }
